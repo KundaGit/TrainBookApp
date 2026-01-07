87eabff57355906159107bca6d05628ec5aa42ab
Kundan Rajak <137685825+KundaGit@users.noreply.github.com>
2026-01-07 15:39:26 +0530

    Change OPENAI_KEY to a new secret value
    Updated OPENAI_KEY with a new value.

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  OPENAI_KEY : '' // ⚠️ visible rahegi
+  OPENAI_KEY : '  OPENAI_KEY : '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' // ⚠️ visible rahegi
+' // ⚠️ visible rahegi
 };
